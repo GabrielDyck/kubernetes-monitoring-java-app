b25b5f75ee816b5f494302625459325018452c8e
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2019-12-02 12:54:02 -0300

    deployment service

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl delete -f deployment.yaml
+kubectl delete -f resources/
--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl apply -f deployment.yaml
+kubectl apply -f resources/
