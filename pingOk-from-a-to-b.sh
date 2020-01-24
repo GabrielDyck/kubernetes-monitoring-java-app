4e9741cee1c367ef8ccb04ad868c2b901c33d857
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2020-01-24 17:06:06 -0300

    replace app to application

--- pingOk-from-a-to-b.sh
+++ pingOk-from-a-to-b.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kubectl exec -n monitoring monitoring-java-web-app-a-6595d99789-52wcs -- curl 10.12.2.70:80/
+kubectl exec -n monitoring monitoring-java-web-app-a-6595d99789-fsbv4 -- curl 10.12.58.164:80/pingError
