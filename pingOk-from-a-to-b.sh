471280b092b2666d476756b6e376f17c9d04e6fa
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2020-02-11 18:34:13 -0300

    test

--- pingOk-from-a-to-b.sh
+++ pingOk-from-a-to-b.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl exec -n monitoring monitoring-java-web-app-a-9b977855b-vzkcb -- curl 10.12.12.194:80/pingOk
+kubectl exec -n monitoring monitoring-java-web-app-a-7789446f57-2mwqs -- curl 10.12.11.197:80/pingOk
